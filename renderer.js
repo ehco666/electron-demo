9afc8bca60e3421163da8b044a1b3879ec724a26
ehco666 <3112218275@qq.com>
2024-03-14 22:33:48 +0800

    first commit

---
+++ renderer.js
@@ -0,0 +1,10 @@
+const information = document.getElementById('info')
+information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`
+
+async function p() {
+	const response = window.versions.ping()
+	console.log(window)
+	console.log(response)
+}
+
+p()
