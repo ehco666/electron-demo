9ee28f4bd5486d028af076976b50a7d6334d13ad
ehco666 <3112218275@qq.com>
2024-03-14 23:32:11 +0800

    two

--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@ const information = document.getElementById('info')
 information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`
 
 async function p() {
-	const response = window.versions.ping()
+	const response = await window.versions.ping()
 	console.log(window)
 	console.log(response)
 }
